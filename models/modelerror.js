c79bd510bf0817ed196f5becb815ad0edb156ad2
Chris Veness <chrisv@movable-type.co.uk>
2015-03-20 13:45:20 +0000

    Use Object.setPrototypeOf() instead of __proto__

--- models/modelerror.js
+++ models/modelerror.js
@@ -17,7 +17,7 @@
  */
 function ModelError(status, message) {
     if (!(this instanceof ModelError)) return new ModelError(status, message);
-    this.constructor.prototype.__proto__ = Error.prototype;
+    Object.setPrototypeOf(this.constructor.prototype, Error.prototype);
     Error.captureStackTrace(this, this.constructor);
     this.name = this.constructor.name;
     this.status = status;
