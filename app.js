70fd1f838d1c9a366e8b76f8fa2b31de3c2df6d2
Chris Veness <chrisv@movable-type.co.uk>
2015-12-02 16:28:17 +0000

    Rationalise use of koa-compose

--- app.js
+++ app.js
@@ -2,13 +2,13 @@
 /* Simple app to explore Node.js + Koa + MySQL basics for CRUD admin + API                        */
 /*                                                                                                */
 /* App comprises three (composed) sub-apps:                                                       */
+/*  - www.   (public website pages)                                                               */
 /*  - admin. (pages for interactively managing data)                                              */
 /*  - api.   (RESTful CRUD API)                                                                   */
-/*  - www.   (public website pages)                                                               */
 /* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -  */
 
 'use strict';
-/* eslint "no-shadow":0 *//* app is already declared in the upper scope */
+/* eslint no-shadow:0 *//* app is already declared in the upper scope */
 
 const koa          = require('koa');               // Koa framework
 const body         = require('koa-body');          // body parser
@@ -25,7 +25,7 @@ const app = module.exports = koa();
 app.use(responseTime());
 
 
-// HTML compression
+// HTTP compression
 app.use(compress({}));
 
 
@@ -44,31 +44,25 @@ GLOBAL.connectionPool = mysql.createPool(config.db); // put in GLOBAL to pass to
 
 
 // select sub-app (admin/api) according to host subdomain (could also be by analysing request.url);
-// qv github.com/koajs/examples/tree/master/vhost
 app.use(function* subApp(next) {
     // use subdomain to determine which app to serve: www. as default, or admin. or api
     const subapp = this.hostname.split('.')[0]; // subdomain = part before first '.' of hostname
 
     switch (subapp) {
         case 'admin':
-            const adminApp = composer(require('./apps/admin/app-admin.js'));
-            return yield adminApp.call(this, next);
+            yield compose(require('./apps/admin/app-admin.js').middleware);
+            break;
         case 'api':
-            const apiApp   = composer(require('./apps/api/app-api.js'));
-            return yield apiApp.call(this, next);
+            yield compose(require('./apps/api/app-api.js').middleware);
+            break;
         case 'www':
-            const wwwApp   = composer(require('./apps/www/app-www.js'));
-            return yield wwwApp.call(this, next);
+            yield compose(require('./apps/www/app-www.js').middleware);
+            break;
         default: // no subdomain? canonicalise hostname to www.hostname
             this.redirect(this.protocol+'://'+'www.'+this.host+this.path+this.search);
             break;
     }
 });
-// composer for sub-apps: github.com/koajs/examples/blob/master/vhost
-function composer(app) {
-    const middleware = app instanceof koa ? app.middleware : app;
-    return compose(middleware);
-}
 
 
 if (!module.parent) {
