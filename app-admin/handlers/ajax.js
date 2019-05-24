d82bfdc78d28dab88e573a04e133b0405b438c40
Chris Veness <chrisv@movable-type.co.uk>
2019-05-24 10:37:41 +0100

    Use class for AjaxHandlers

--- app-admin/handlers/ajax.js
+++ app-admin/handlers/ajax.js
@@ -12,44 +12,50 @@
 
 import fetch from 'node-fetch'; // window.fetch in node.js
 
-const handler = {};
+
+class AjaxHandlers {
+
+    /*
+     * Routes/handlers for app-specific ajax calls can go here; this sample app has none.
+     */
 
 
-/*
- * Routes/handlers for app-specific ajax calls can go here; this sample app has none.
- */
+    /*
+     * This provides an interface to the 'api' app, hence providing a RESTful-structured ajax service;
+     * e.g. GET admin.app.com/ajax/members/123456 => GET api.app.com/members/123456
+     *
+     * It uses the same JSON Web Token to authenticate as was used for website sign-in.
+     */
+    static async ajaxApiPassthrough(ctx) {
+        const resource = ctx.request.url.replace('/ajax/', '');
+        const host = ctx.request.host.replace('admin', 'api');
+        const url = ctx.request.protocol + '://' + host + '/' + resource;
 
+        const body = JSON.stringify(ctx.request.body) == '{}' ? null : JSON.stringify(ctx.request.body);
+        const hdrs = {
+            'Content-Type': 'application/json',
+            'Accept': ctx.request.header.accept || '*/*',
+            'Authorization': 'Bearer ' + ctx.state.user.jwt,
+        };
 
-/*
- * This provides an interface to the 'api' app, hence providing a RESTful-structured ajax service;
- * e.g. GET admin.app.com/ajax/members/123456 => GET api.app.com/members/123456
- *
- * It uses the same JSON Web Token to authenticate as was used for website sign-in.
- */
-handler.ajaxApiPassthrough = async function(ctx) {
-    const resource = ctx.request.url.replace('/ajax/', '');
-    const host = ctx.request.host.replace('admin', 'api');
-    const url = ctx.request.protocol+'://'+host+'/'+resource;
-
-    const body = JSON.stringify(ctx.request.body)=='{}' ? null : JSON.stringify(ctx.request.body);
-    const hdrs = {
-        'Content-Type':  'application/json',
-        'Accept':        ctx.request.header.accept || '*/*',
-        'Authorization': 'Bearer '+ctx.state.user.jwt,
-    };
-
-    try {
-        const response = await fetch(url, { method: ctx.request.method, body: body, headers: hdrs });
-        const json = response.headers.get('content-type').match(/application\/json/);
-        ctx.response.status = response.status;
-        ctx.response.body = json ? await response.json() : await response.text();
-    } catch (e) { // eg offline, DNS fail, etc
-        ctx.response.status = 500;
-        ctx.response.body = e.message;
+        try {
+            const response = await fetch(url, {
+                method:  ctx.request.method,
+                body:    body,
+                headers: hdrs
+            });
+            const json = response.headers.get('content-type').match(/application\/json/);
+            ctx.response.status = response.status;
+            ctx.response.body = json ? await response.json() : await response.text();
+        } catch (e) { // eg offline, DNS fail, etc
+            ctx.response.status = 500;
+            ctx.response.body = e.message;
+        }
     }
-};
+
+}
 
 
 /* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -  */
 
-export default handler;
+export default AjaxHandlers;
